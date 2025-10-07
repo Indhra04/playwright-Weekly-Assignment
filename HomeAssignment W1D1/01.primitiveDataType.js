aec89a4d4754f84be1c8ea81605e497369be232f
Indhira Piriyadharsini <81368121+Indhra04@users.noreply.github.com>
2025-10-07 22:39:01 +0530

    Add files via upload

---
+++ HomeAssignment W1D1/01.primitiveDataType.js
@@ -0,0 +1,21 @@
+
+let firstName = "indhira"
+let companyName = "Cognizant"
+let mobileNumber = 9943037783
+let isAutomation = true;
+let hasPlaywright;
+
+console.log(firstName);
+console.log(`The Datatype of firstname is ${typeof(firstName)}`);
+
+console.log(companyName);
+console.log(`The Datatype of companyName is ${typeof(companyName)}`);
+
+console.log(mobileNumber);
+console.log(`The Datatype of mobileNumber is ${typeof(mobileNumber)}`);
+
+console.log(isAutomation);
+console.log(`The Datatype of isAutomation is ${typeof(isAutomation)}`);
+
+console.log(hasPlaywright);
+console.log(`The Datatype of hasPlaywright is ${typeof(hasPlaywright)}`);
